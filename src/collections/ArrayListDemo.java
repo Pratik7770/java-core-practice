c203f5d586324a0e7e8b9fc0abcfcf9ebc133e55
Pratik7770 <pratikpawar7770@gmail.com>
2025-12-24 18:38:52 +0530

    Initial Java collections practice

---
+++ src/collections/ArrayListDemo.java
@@ -0,0 +1,14 @@
+package collections;
+
+import java.util.*;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<String> cities = new ArrayList<>();
+        cities.add("Pune");
+        cities.add("Mumbai");
+        cities.add("Delhi");
+
+        System.out.println(cities);
+    }
+}
