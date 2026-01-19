131c467845c6087c6278c6800656563cafc0d543
Pratik7770 <pratikpawar7770@gmail.com>
2026-01-19 20:05:37 +0530

    Added pratik name

---
+++ src/basics/PratikPattern.java
@@ -0,0 +1,65 @@
+package basics;
+public class PratikPattern {
+    public static void main(String[] args) {
+        int n = 7; // pattern height
+
+        for (int i = 0; i < n; i++) {
+
+            // ----- P -----
+            for (int j = 0; j < n; j++) 
+            {
+                if (j == 0 || (i == 0 || i == n / 2) && j < n - 1 || (j == n - 1 && i < n / 2 && i != 0))
+                    System.out.print("#");
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("   ");
+
+            // ----- R -----
+            for (int j = 0; j < n; j++) {
+                if (j == 0 || (i == 0 || i == n / 2) && j < n - 1 || (j == n - 1 && i != 0 && i < n / 2) || (i - j == n / 2))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("   ");
+
+            // ----- A -----
+            for (int j = 0; j < n; j++) {
+                if ((i == 0 && j != 0 && j != n - 1) || (i == n / 2) || j == 0 || j == n - 1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("   ");
+
+            // ----- T -----
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == n / 2)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("   ");
+
+            // ----- I -----
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == n - 1 || j == n / 2)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.print("   ");
+
+            // ----- K -----
+            for (int j = 0; j < n; j++) {
+                if (j == 0 || i + j == n / 2 || i - j == n / 2)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
